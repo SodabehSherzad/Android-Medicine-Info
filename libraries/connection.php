95426ce1de536e784b9327699a0b305386a34339
Abdul Rahman Sherzad <absherzad@gmail.com>
2020-08-31 15:54:00 +0200

    connection.php has been re-factored

--- libraries/connection.php
+++ libraries/connection.php
@@ -1,7 +1,18 @@
 <?php
-	require_once "config/config.php";
 
-	$link = @mysqli_connect(HOST,USERNAME,PASSWORD, DATABASE);
-	if(!$link) {
-		die('I cannot connect to MySQL Server. ' . mysqli_connect_error());
+	// Set the database access information as constants.
+	define("HOST", "localhost:3306");
+	define("USERNAME", "root");
+	define("PASSWORD", "");
+	define("DATABASE", "medicineinfo");
+	
+	// Make the connection.
+	$GLOBALS['DB'] = @mysqli_connect(HOST,USERNAME,PASSWORD, DATABASE);
+	if (!$GLOBALS['DB']) {
+	  echo 'Error No. ' . mysqli_connect_errno() . PHP_EOL . '<br>';
+	  echo 'Error: ' . mysqli_connect_error() . PHP_EOL . '<br>';
+	  exit('Error: Unable to connect to MySQL' . PHP_EOL);
 	}
+	
+	// Set proper character sets
+	mysqli_set_charset($GLOBALS['DB'], 'utf8');
