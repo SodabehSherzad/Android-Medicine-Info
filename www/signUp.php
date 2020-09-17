8571d0ee5b0b2af61239706da8fd97bc5c834d3c
Abdul Rahman Sherzad <absherzad@gmail.com>
2020-09-17 20:05:08 +0200

    SignUp.php validation improved using GUMP validation classs

--- www/signUp.php
+++ www/signUp.php
@@ -1,116 +1,69 @@
-<?php 
+<?php
 require_once "../libraries/connection.php";
-$title = "Checkout"; 
-require_once("./includes/header.php");
+$title = "Checkout";
+require_once "./includes/header.php";
+require_once '../vendor/autoload.php';
+
 $passwordErr = "";
-$errors = array("fname" => "", "lname"=>"", "fnamecharacter" => "", "lnamecharacter"=>"", "username"=>"", "password"=>"", "usernamecharacter"=>"", "passwordcharacter"=>"","confirm_password"=>"", "email"=>"", "phone"=>"", "address"=>"", "country"=>"", "phonecharacter"=>"", "addresscharacter"=>"", "countrycharacter"=>"");
+$validation_msgs = null;
 $fname = $lname = $username = $password = $confirm_password = $email = $address = $country = $note = $phone = "";
-if(isset($_POST['btnSignUp'])){
-  $fname = $_POST['fname'];
-  $lname = $_POST['lname'];
-  $password = $_POST['password'];
-  $confirm_password = $_POST['confirm_password'];
-  $email = $_POST['email'];
-  $country = $_POST['country']; 
-  $phone = $_POST['phone'];
-  $address = $_POST['address'];
-  $username = $_POST['username'];
-  $note = $_POST['note'];
+if (isset($_POST['btnSignUp'])) {
 
-  if(empty($fname)){
-    $errors["fname"] = "First Name can not be empty!"; 
-  }else if(strlen(trim($fname)) < 3 || strlen(trim($fname)) > 55){
-    $errors["fnamecharacter"] = "First Name must be between 3 and 55 characters!"; 
-  }else{
-    $errors["fname"] = $errors["fnamecharacter"] = "";
-  }
+    // Form validation
+    $validator = new GUMP();
 
-  if(empty($lname)){
-    $errors["lname"] = "Last Name can not be empty!"; 
-  }else if(strlen(trim($lname)) < 3 || strlen(trim($lname)) > 55){
-    $errors["lnamecharacter"] = "Last Name must be between 3 and 55 characters!"; 
-  }else{
-    $errors["lname"] = $errors["lnamecharacter"] = "";
-  }
-  
-  if(empty($password)){
-    $errors["password"] = "Password can not be empty!"; 
-  }else if(strlen(trim($password)) < 3 || strlen(trim($password)) > 55){
-    $errors["passwordcharacter"] = "Password must be between 3 and 55 characters!"; 
-  }else{
-    $errors["password"] = $errors["passwordcharacter"] = "";
-  }
+    // Define the rules
+    $rules = [
+        'fname' => 'required|max_len,55|min_len,3',
+        'lname' => 'required|max_len,55|min_len,3',
+        'password' => 'required|max_len,55|min_len,6',
+        'confirm_password' => 'required',
+        'email' => 'required|valid_email',
+        'phone' => 'required|numeric|max_len,20|min_len,10',
+        'address' => 'required',
+        'username' => 'required|numeric|max_len,55|min_len,6',
+        'note' => 'required',
+    ];
 
-  if(empty($confirm_password)){
-    $errors["confirm_password"] = "Confirm Password can not be empty!"; 
-  }else{
-    $errors["confirm_password"] = "";
-  }
+    // Define the filters
+    $filters = [
+        'fname' => 'trim|sanitize_string',
+        'lname' => 'trim|sanitize_string',
+        'password' => 'trim|sanitize_string',
+        'confirm_password' => 'trim|sanitize_string',
+        'email' => 'trim|sanitize_string',
+        'phone' => 'trim|sanitize_string',
+        'address' => 'trim|sanitize_string',
+        'username' => 'trim|sanitize_string',
+        'note' => 'trim|sanitize_string',
+    ];
 
-  if(empty($email)){
-    $errors["email"] = "email can not be empty!"; 
-  }else{
-    $errors["email"] = "";
-  }
-  
-  if(empty($phone)){
-    $errors["phone"] = "Phone can not be empty!"; 
-  }else if(strlen(trim($phone)) < 10 || strlen(trim($phone)) > 20){
-    $errors["phonecharacter"] = "Phone must be between 10 and 20 numbers!"; 
-  }else{
-    $errors["phone"] =  $errors["phonecharacter"] = "";
-  }
+    $_POST = $validator->filter($_POST, $filters);
+    $validated = $validator->is_valid($_POST, $rules);
 
+    if ($validated === true) {
+        $fname = mysqli_real_escape_string($GLOBALS['DB'], $_POST['fname']);
+        $lname = mysqli_real_escape_string($GLOBALS['DB'], $_POST['lname']);
+        $password = mysqli_real_escape_string($GLOBALS['DB'], $_POST['password']);
+        $confirm_password = mysqli_real_escape_string($GLOBALS['DB'], $_POST['confirm_password']);
+        $email = mysqli_real_escape_string($GLOBALS['DB'], $_POST['email']);
+        $country = mysqli_real_escape_string($GLOBALS['DB'], $_POST['country']);
+        $phone = mysqli_real_escape_string($GLOBALS['DB'], $_POST['phone']);
+        $address = mysqli_real_escape_string($GLOBALS['DB'], $_POST['address']);
+        $username = mysqli_real_escape_string($GLOBALS['DB'], $_POST['username']);
+        $note = mysqli_real_escape_string($GLOBALS['DB'], $_POST['note']);
 
-  if(empty($address)){
-    $errors["address"] = "Address can not be empty!"; 
-  }else if(strlen(trim($address)) < 10 || strlen(trim($address)) > 55){
-    $errors["addresscharacter"] = "Address must be between 10 and 55 characters!"; 
-  }else{
-    $errors["address"] = $errors["addresscharacter"] = "";
-  }
-
-  if(empty($country)){
-    $errors["country"] = "Country can not be empty!"; 
-  }else if(strlen(trim($country)) < 5 || strlen(trim($country)) > 55){
-    $errors["countrycharacter"] = "country must be between 5 and 55 characters!"; 
-  }else{
-    $errors["country"]= $errors["countrycharacter"] = "";
-  }
-
-  if(empty($username)){
-    $errors["username"] = "Username can not be empty!"; 
-  }else if(strlen(trim($username)) < 6 || strlen(trim($username)) > 55){
-    $errors["usernamecharacter"] = "Username must be between 6 and 55 characters!"; 
-  }else{ 
-    $errors["username"] = $errors["usernamecharacter"] = "";
-  }
-
-  if(strcmp($password, $confirm_password) !== 0){
-    $passwordErr = "Password does not match";
-  }else{
-    $passwordErr = "";
-  }
-
-  if(empty($errors["fname"]) && empty($errors["lname"]) && empty($errors["username"]) 
-        && empty($errors["email"]) && empty($errors["country"]) && empty($errors["address"]) &&
-        empty($errors["phone"]) && empty($errors["password"]) && empty($errors["confirm_password"]) && empty($passwordErr)
-        && empty($errors["usernamecharacter"]) && empty($errors["countrycharacter"]) && empty($errors["addresscharacter"]) && empty($errors['phonecharacter'])
-        && empty($errors['passwordcharacter']) && empty($errors['lnamecharacter'])&& empty($errors['fnamecharacter'])){
-    
         //before insert we will check that exist user or email similar if exit we show err else insert
 
         $sql = "INSERT INTO users VALUES(NULL, '$fname', '$lname', '$username', '$address', '$email', '$country', '$phone', '$note', '$password')";
         $result = mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error($GLOBALS['DB']));
-          
-  }
-  // else {
-  //   echo "Emptyyyyy";
-  // }
-
-  //  "<pre>".print_r($errors)."</pre>";
-
-  //  "<pre>".print_r($_POST)."</pre>";
+    } else {
+        $validation_msgs = $validated;
+        //echo "<pre>";
+        //print_r($validated);
+        //echo "</pre>";
+        //die();
+    }
 
 }
 
@@ -118,11 +71,22 @@ if(isset($_POST['btnSignUp'])){
 <body>
 
   <div class="site-wrap">
-  <?php require_once("./includes/navbar.php")?>
+  <?php require_once "./includes/navbar.php"?>
 
     <div class="bg-light py-3">
       <div class="container">
         <div class="row">
+        <?php if (isset($validation_msgs)): ?>
+              <div class="alert alert-warning">
+                <h4 class="alert-heading h5">خطای اعتبار سنجی اطلاعات</h4>
+                <ul>
+                <?php foreach ($validation_msgs as $msg): ?>
+                  <li><?=$msg?></li>
+                <?php endforeach;?>
+                </ul>
+              </div>
+          <?php endif;?>
+
           <div class="col-md-12 mb-0">
             <a href="index.php">Home</a> <span class="mx-2 mb-0">/</span>
             <strong class="text-black">Create Account</strong>
@@ -147,7 +111,7 @@ if(isset($_POST['btnSignUp'])){
               <div class="form-group">
                 <label for="country" class="text-black">Country <span class="text-danger">*</span></label>
                 <select id="country" class="form-control" name="country">
-                  <option value="<?= $country?>">Select a country</option>
+                  <option value="<?=$country?>">Select a country</option>
                   <option value="Bangladesh">Bangladesh</option>
                   <option value="Algeria">Algeria</option>
                   <option value="Afghanistan">Afghanistan</option>
@@ -157,67 +121,50 @@ if(isset($_POST['btnSignUp'])){
                   <option value="Colombia">Colombia</option>
                   <option value="Dominican Republic">Dominican Republic</option>
                 </select>
-                <label for="country" class="text-black"><span class="text-danger"><?= $errors['country']?></span></label>
-                <label for="country" class="text-black"><span class="text-danger"><?= $errors['countrycharacter']?></span></label>
               </div>
               <div class="form-group row">
                 <div class="col-md-6">
                   <label for="fname" class="text-black">First Name <span class="text-danger">*</span></label>
                   <input type="text" class="form-control" id="fname" name="fname" value="<?=$fname?>">
-                  <label for="fname" class="text-black"><span class="text-danger"><?= $errors['fname'];?> </span></label>
-                  <label for="fname" class="text-black"><span class="text-danger"><?= $errors['fnamecharacter'];?> </span></label>
                 </div>
                 <div class="col-md-6">
                   <label for="lname" class="text-black">Last Name <span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" id="lname" name="lname" value="<?= $lname?>">
-                  <label for="lname" class="text-black"><span class="text-danger"><?= $errors['lname'];?> </span></label>
-                  <label for="lname" class="text-black"><span class="text-danger"><?= $errors['lnamecharacter'];?> </span></label>
+                  <input type="text" class="form-control" id="lname" name="lname" value="<?=$lname?>">
                 </div>
               </div>
-    
+
               <div class="form-group row">
                 <div class="col-md-12">
                   <label for="username" class="text-black">User Name <span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" id="username" name="username" value="<?= $username?>">
-                  <label for="username" class="text-black"><span class="text-danger"><?= $errors['username'];?></span></label>       
-                  <label for="username" class="text-black"><span class="text-danger"><?= $errors['usernamecharacter'];?></span></label>       
+                  <input type="text" class="form-control" id="username" name="username" value="<?=$username?>">
                 </div>
               </div>
 
               <div class="form-group row">
                 <div class="col-md-6">
                   <label for="password" class="text-black">Password <span class="text-danger">*</span></label>
-                  <input type="password" class="form-control" id="password" name="password" value="<?= $password?>">
-                  <label for="password" class="text-black"><span class="text-danger"><?= $errors['password']?></span></label>
-                  <label for="password" class="text-black"><span class="text-danger"><?= $errors['passwordcharacter']?></span></label>
+                  <input type="password" class="form-control" id="password" name="password" value="<?=$password?>">
                 </div>
                 <div class="col-md-6">
                   <label for="confirm_password" class="text-black">Confirm Password <span class="text-danger">*</span></label>
-                  <input type="password" class="form-control" id="confirm_password" name="confirm_password" value="<?= $confirm_password?>">
-                  <label for="password" class="text-black"><span class="text-danger"><?= $errors['confirm_password']?></span></label>
-                  <label for="password" class="text-black"><span class="text-danger"><?= $passwordErr?></span></label>
+                  <input type="password" class="form-control" id="confirm_password" name="confirm_password" value="<?=$confirm_password?>">
                 </div>
               </div>
-    
+
               <div class="form-group row">
                 <div class="col-md-12">
                   <label for="address" class="text-black">Address <span class="text-danger">*</span></label>
-                  <input type="text" class="form-control" id="address" name="address" placeholder="Street address" value="<?= $address?>">
-                  <label for="address" class="text-black"><span class="text-danger"><?= $errors['address']?></span></label>
-                  <label for="address" class="text-black"><span class="text-danger"><?= $errors['addresscharacter']?></span></label>
+                  <input type="text" class="form-control" id="address" name="address" placeholder="Street address" value="<?=$address?>">
                 </div>
               </div>
               <div class="form-group row mb-5">
                 <div class="col-md-6">
                   <label for="email" class="text-black">Email Address <span class="text-danger">*</span></label>
-                  <input type="email" class="form-control" id="email" name="email" value="<?= $email?>">
-                  <label for="email" class="text-black"><span class="text-danger"><?= $errors['email']?></span></label>
+                  <input type="email" class="form-control" id="email" name="email" value="<?=$email?>">
                 </div>
                 <div class="col-md-6">
                   <label for="phone" class="text-black">Phone <span class="text-danger">*</span></label>
                   <input type="number" class="form-control" id="phone" name="phone" placeholder="Phone Number" value="<?=$phone?>">
-                  <label for="phone" class="text-black"><span class="text-danger"><?= $errors['phone']?></span></label>
-                  <label for="phone" class="text-black"><span class="text-danger"><?= $errors['phonecharacter']?></span></label>
                 </div>
               </div>
               <div class="form-group">
@@ -232,12 +179,12 @@ if(isset($_POST['btnSignUp'])){
                 </div>
             </form>
           </div>
-         
+
         </div>
         <!-- </form> -->
       </div>
     </div>
-    
+
 
     <div class="site-section bg-secondary bg-image" style="background-image: url('images/bg_2.jpg');">
       <div class="container">
@@ -263,4 +210,4 @@ if(isset($_POST['btnSignUp'])){
         </div>
       </div>
     </div>
-    <?php require_once("./includes/footer.php")?>
+    <?php require_once "./includes/footer.php"?>
